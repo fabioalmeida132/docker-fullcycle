aaa893f5d4f6273a8fca751b1b9b774fb68430ad
fabio <fabio@DESKTOP-TC0FHJD.localdomain>
2022-11-05 20:49:48 -0300

    desafio nginx com mysql

---
+++ 2 - Nginx com Node.JS/node/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb',
+}
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Fábio Almeida')`;
+connection.query(sql);
+connection.end();
+
+app.get('/', (req, res) => {
+    const connection = mysql.createConnection(config);
+    const sql = `SELECT name FROM people`;
+    connection.query(sql, function(error, results, fields) {
+        res.send(`
+            <h1>Full Cycle Rocks!</h1>
+            <ul>
+                ${results.map(result => `<li>${result.name}</li>`).join('')}
+            </ul>
+        `);
+    });
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
