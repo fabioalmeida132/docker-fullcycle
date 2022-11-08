39ef923c94ab1cccbb8ca0daa45c827c1e451719
fabio <fabio@DESKTOP-TC0FHJD.localdomain>
2022-11-07 21:55:01 -0300

    fix: adicionado initdb para iniciar o banco com a tabela people, e ajustado node com dockerize

--- 2 - Nginx com Node.JS/node/index.js
+++ 2 - Nginx com Node.JS/node/index.js
@@ -28,4 +28,4 @@ app.get('/', (req, res) => {
     });
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`App rodando na porta ${port}!`))
